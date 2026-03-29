e21951a244d09a9b9f06cb0b578a37e4cafe493b
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-03-29 13:16:32 +0200

    [uodate] - LockBox with PriceConverter & AggregatorV3

--- src/LockBox.sol
+++ src/LockBox.sol
@@ -1,9 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "src/PriceConverter.sol";
+
 contract LockBox {
+    /* Initialise library */
+    using PriceConverter for uint256;
+
     /* Errors */
-    error LockBox__NotEnoughSent();
+    error LockBox__NotEnoughDuration();
 
     /* Type declarations */
     enum State {
@@ -19,7 +25,8 @@ contract LockBox {
 
     /* State variables */
     address immutable i_owner;
-    uint256 private constant MIN_AMOUNT = 1 ether;
+    AggregatorV3Interface private s_priceFeed;
+    uint256 private constant MIN_AMOUNT = 200e18;
     uint256 private constant MIN_LOCK_DURATION = 7 days;
     uint256 private constant MIN_EXTENSION = 7 days;
     mapping(address => DepositInfo) private s_depositInfo;
@@ -28,12 +35,22 @@ contract LockBox {
     event NewDeposit(address indexed sender);
 
     /* Constructor */
-    constructor() {
+    constructor(AggregatorV3Interface priceFeed) {
         i_owner = msg.sender;
+        s_priceFeed = priceFeed;
     }
 
     /* Functions */
-    function deposit(uint256 lockTime) external {}
+    function deposit(uint256 lockTime) external {
+        // Checks
+        if (lockTime < MIN_LOCK_DURATION) {
+            revert LockBox__NotEnoughDuration();
+        }
+
+        // Effects
+
+        // Interactions
+    }
 
     /* Getter functions */
     function getOwner() external view returns (address) {
