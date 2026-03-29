ac4a9c5f07c43806149eae38843ec332d1964201
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-03-29 13:09:21 +0200

    [add] - Price converter library

--- src/PriceConverter.sol
+++ src/PriceConverter.sol
@@ -1,4 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-library PriceConverter {}
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    /* Functions */
+    function getPrice(AggregatorV3Interface priceFeed) internal view returns (uint256) {
+        // Get price from Chainlink contract
+        (, int256 price,,,) = priceFeed.latestRoundData();
+
+        // Cast to uint256 and multiply by 1e10 to match eth in wei
+        return uint256(price * 1e10);
+    }
+
+    function priceConverter(uint256 ethAmount, AggregatorV3Interface priceFeed) internal view returns (uint256) {
+        // Get the current price
+        uint256 price = getPrice(priceFeed);
+
+        // Convert the price
+        uint256 ethAmountInUsd = (price * ethAmount) / 1e18;
+
+        // Return the price
+        return ethAmountInUsd;
+    }
+}
